c642b19cbed75d9bddd2def4d833c778a6e131e1
Skyffyy <Skyffiriks@gmail.com>
2026-06-23 22:48:08 +0300

    added database draft and frontend wireframe layout

--- 03_database/2-14_schema.sql
+++ 03_database/2-14_schema.sql
@@ -1,11 +1,26 @@
--- Create project-specific database schema here.
--- Student must update this file during implementation.
+-- Draft of tables for Week 1
 
+-- 1. Users table
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    full_name VARCHAR(120) NOT NULL,
-    email VARCHAR(160) NOT NULL UNIQUE,
-    role VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL DEFAULT 'User', -- Admin, User, Guest
+    status VARCHAR(20) NOT NULL DEFAULT 'Active'
+);
+
+-- 2. Chat Rooms table
+CREATE TABLE IF NOT EXISTS rooms (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    is_private BOOLEAN DEFAULT FALSE
+);
+
+-- 3. Messages table
+CREATE TABLE IF NOT EXISTS messages (
+    id SERIAL PRIMARY KEY,
+    room_id INT REFERENCES rooms(id) ON DELETE CASCADE,
+    user_id INT REFERENCES users(id) ON DELETE SET NULL,
+    message_text TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
